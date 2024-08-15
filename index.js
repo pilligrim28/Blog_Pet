ae1d32d54e16027727f1ea0b638e8d3839558ec5
pilligrim28 <balu-133@ya.ru>
2024-08-15 13:28:03 +0300

    Создание БД

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 
-import {registerValidation} from './validations/auth.js'
+import { registerValidation } from './validations/auth.js'
 import { validationResult } from 'express-validator';
 
 import UserModel from './models/User.js';
 
 mongoose
-    .connect('mongodb+srv://jobanikolaev:Batyrevo1@myblog.j9ox3.mongodb.net/?retryWrites=true&w=majority&appName=MyBlog')
+    .connect('mongodb+srv://jobanikolaev:Batyrevo1@myblog.j9ox3.mongodb.net/blog?retryWrites=true&w=majority&appName=MyBlog')
     .then(() => console.log('DB ok'))
     .catch(() => console.log('DB Error', err));
 
@@ -22,21 +23,28 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-app.post('/auth/register', registerValidation, (req, res) => {
-const errors = validationResult(req);
-if (!errors.isEmpty()){
-    return res.status(400).json(errors.array());
-}
-const doc =new UserModel({
-email: req.body.email,
-fullName: req.body.fullName,
-avatarUrl: req.body.avatarUrl,
-password: req.body.password,
-});
+app.post('/auth/register', registerValidation, async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    }
+    const password = req.body.password;
 
-res.json({
-    success: true,
-});
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash,
+    });
+
+    const user = await doc.save();
+
+    res.json({
+
+    });
 
 });
 
